82b053b4f86ab43538a6efb10ae610e615ae29d0
周公来 <zhougonglai@dingtalk.com>
2019-08-30 08:40:56 +0800

    type: init

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,24 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>WebRTC</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn text>
+        <span class="mr-2">star</span>
       </v-btn>
     </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      <router-view />
     </v-content>
   </v-app>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+  name: "App",
   data: () => ({
-    //
+    device: "user-agient"
   }),
+  methods: {}
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,46 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1 class="title">RealTime communication with WebRTC</h1>
+    <video autoplay playsinline ref="video" />
+    <v-btn text @click="getUserMedia">
+      <span class="mr-2">star</span>
+    </v-btn>
+    <v-btn text @click="stopSteam">
+      <span class="mr-2">stop</span>
+    </v-btn>
+  </div>
 </template>
-
 <script>
-import HelloWorld from '../components/HelloWorld';
-
 export default {
-  components: {
-    HelloWorld,
+  name: "home",
+  data: () => ({
+    mediaSteam: "",
+    videoTracks: ""
+  }),
+  methods: {
+    getUserMedia() {
+      navigator.mediaDevices
+        .getUserMedia({
+          audio: true,
+          video: true
+        })
+        .then(mediaSteam => {
+          this.mediaSteam = mediaSteam;
+          this.videoTracks = mediaSteam.getVideoTracks();
+          if ("srcObject" in this.$refs.video) {
+            this.$refs.video.srcObject = stream;
+          } else {
+            this.$refs.video.src = window.URL.createObjectURL(stream);
+          }
+          this.$refs.video.onloadedmetadata = function(e) {
+            this.$refs.video.play();
+          };
+        });
+    },
+    stopSteam() {
+      this.mediaSteam.stop();
+    }
   },
+  created() {}
 };
 </script>
